9baee04f738f0132a8ecb30dd6c2006cc04082f1
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-07-01 21:35:05 +0530

    Create Number of 1 Bits.cpp

---
+++ Number of 1 Bits.cpp
@@ -0,0 +1,9 @@
+    int setBits(int N) {
+        // Write Your Code here
+        int ans=0;
+        while(N){
+            if(N&1)ans++;
+            N>>=1;
+        }
+        return ans;
+    }
