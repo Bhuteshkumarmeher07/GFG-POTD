9a55c0a9a5bcd861addf8ab54f557cf0e85992eb
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-07-08 12:44:35 +0530

    Create Find triplets with zero sum.cpp

---
+++ Find triplets with zero sum.cpp
@@ -0,0 +1,27 @@
+    bool findTriplets(int arr[], int n)
+    { 
+        //Your code here
+        sort(arr, arr+1);
+        for(int i =0; i<n; i++)
+        {
+            int j = i+1;
+            int k = n-1;
+            
+            while(j<k)
+            {
+                if(arr[i] + arr[j] + arr[k] > 0)
+                {
+                    k--;
+                }
+                else if(arr[i] + arr[j] + arr[k] < 0)
+                {
+                    j++;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
