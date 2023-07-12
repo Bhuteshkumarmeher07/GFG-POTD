2f61f1cb0835701ca4aeefd467eba56f84af1f8b
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-07-12 13:16:35 +0530

    Create Power Of Numbers.cpp

---
+++ Power Of Numbers.cpp
@@ -0,0 +1,16 @@
+    public:
+    //You need to complete this fucntion
+    int mod = 1000000007;
+    
+    long long power(int N,int R)
+    {
+       //Your code here
+       if(R==0) return 1;
+       
+       long long curr_ans = power(N,R/2);
+       
+       long long ans = ((curr_ans%mod)*(curr_ans%mod))%mod;
+       
+       if(R%2 == 0) return ans;
+       else return ((ans%mod) * (N%mod))%mod;
+    }
