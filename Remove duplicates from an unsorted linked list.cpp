4de6e18a3a27b65ebfe7015149964eb1c96b4fad
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-07-22 12:45:05 +0530

    Create Remove duplicates from an unsorted linked list.cpp

---
+++ Remove duplicates from an unsorted linked list.cpp
@@ -0,0 +1,20 @@
+    Node * removeDuplicates( Node *head) 
+    {
+        vector<int> f(1e4 + 10, 0);
+        
+        Node * prev = nullptr;
+        Node * cur = head;
+        
+        while(cur != nullptr){
+            ++f[cur -> data];
+            
+            if(f[cur -> data] > 1)
+                prev -> next = cur -> next;
+            else
+                prev = cur;
+            
+            cur = cur -> next;
+        }
+        
+        return head;
+    }
