cd993d7a074a8ed3644f5e87621fdc22661eb8ff
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-09-05 21:39:06 +0530

    Create Print adjacency list.cpp

---
+++ Print adjacency list.cpp
@@ -0,0 +1,10 @@
+    vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
+        vector<vector<int>> g(V);
+        
+        for(auto i : edges){
+            g[i.first].push_back(i.second);
+            g[i.second].push_back(i.first);
+        }
+        
+        return g;
+    }
