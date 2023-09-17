1e87a42c99038f90a5e4245fa94b5abacdc7ef4b
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-09-17 11:18:11 +0530

    Create Print first n Fibonacci Numbers.cpp

---
+++ Print first n Fibonacci Numbers.cpp
@@ -0,0 +1,13 @@
+    vector<long long> printFibb(int n) {
+        vector<long long> M;
+        if(n==1){
+            M.push_back(1);
+            return M;
+        }
+        M.push_back(1);
+        M.push_back(1);
+        for(int i=2;i<n;i++){
+            M.push_back(M[i-1]+M[i-2]);
+        }
+        return M;
+    }
