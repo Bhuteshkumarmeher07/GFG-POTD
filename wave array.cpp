51dc7b54edab4a610985ea0000b19dfedfef5746
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-09-28 17:22:28 +0530

    Create wave array.cpp

---
+++ wave array.cpp
@@ -0,0 +1,8 @@
+    void convertToWave(int n, vector<int>& arr){
+        for(int i=0; i<n-1; i+=2)
+        {
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+    }
