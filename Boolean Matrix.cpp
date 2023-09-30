5b96b536cf03b968fd0293cd7e457f50a5d17c4e
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-09-30 10:08:32 +0530

    Create Boolean Matrix.cpp

---
+++ Boolean Matrix.cpp
@@ -0,0 +1,31 @@
+    void booleanMatrix(vector<vector<int> > &matrix)
+    {
+        int n=matrix.size(),m=matrix[0].size();
+        
+        bool rows[n];
+        bool cols[m];
+        
+        for(int i=0;i<n;i++) rows[i]=false;
+        for(int j=0;j<m;j++) cols[j]=false;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==1)
+                {
+                    rows[i]=true; cols[j]=true;
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(rows[i] or cols[j]) matrix[i][j]=1;
+            }
+        }
+        
+
+    }
