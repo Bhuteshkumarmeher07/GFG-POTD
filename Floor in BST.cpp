4711d6d31c06e6e568704c6a2170daba669a496c
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-10-13 17:30:12 +0530

    Create Floor in BST.cpp

---
+++ Floor in BST.cpp
@@ -0,0 +1,15 @@
+    int floor(Node* root, int x) {
+        int res = -1;
+
+        while (root) {
+            if (root->data == x)
+                return x;
+            if (root->data > x)
+                root = root->left;
+            else {
+                res = root->data;
+                root = root->right;
+            }
+        }
+        return res;
+    }
