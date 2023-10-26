47a7e5d80feaf73bba6db1486680d46b5c0a7f2e
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-10-26 10:59:04 +0530

    Create Minimum Operations.cpp

---
+++ Minimum Operations.cpp
@@ -0,0 +1,13 @@
+    public int minOperation(int n)
+    {
+        int count = 0 ;
+        while(n > 0)
+        {
+            if(n % 2 == 0)
+            n = n / 2 ;
+            else
+            n-- ;
+            count++;
+        }
+        return count;
+    }
