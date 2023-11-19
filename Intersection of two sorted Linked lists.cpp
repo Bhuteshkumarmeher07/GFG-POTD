b2d77af39a78acbd420c36a61e4ecd0f196ad6c9
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-11-19 09:38:44 +0530

    Create Intersection of two sorted Linked lists.cpp

---
+++ Intersection of two sorted Linked lists.cpp
@@ -0,0 +1,28 @@
+    Node* findIntersection(Node* head1, Node* head2)
+    {
+        if(!head1)
+            return head2;
+            
+        if(!head2)
+            return head1;
+            
+        Node *dummy = new Node(0);
+        Node *temp = dummy;
+        
+        while(head1 and head2) 
+        {
+            if(head1->data == head2->data) 
+            {
+                temp->next = new Node(head1->data);
+                head1 = head1->next;
+                head2 = head2->next;
+                temp = temp->next;
+            } 
+            else if(head1->data < head2->data)
+                head1 = head1->next;
+            else
+                head2 = head2->next;
+        }
+        
+        return dummy->next;
+    }
