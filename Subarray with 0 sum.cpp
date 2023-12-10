6f992f404208fcf0c047eb07f68c3f526798b206
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-12-10 13:57:07 +0530

    Create Subarray with 0 sum.cpp

---
+++ Subarray with 0 sum.cpp
@@ -0,0 +1,20 @@
+bool subArrayExists(int arr[], int n)
+    {
+        //Your code here
+        unordered_map<int,int>mp;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            
+            if(sum==0) return true;
+            
+            if(arr[i]==0) return true;
+            
+            else if(mp.find(sum)!=mp.end()) return true;
+            
+            else {
+                mp[sum]=i;
+            }
+        }
+        return false;
+    }
