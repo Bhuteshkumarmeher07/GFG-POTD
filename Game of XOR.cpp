70dfa1ad62b0f46bc617004db014a935a343b087
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2023-12-18 13:22:55 +0530

    Create Game of XOR.cpp

---
+++ Game of XOR.cpp
@@ -0,0 +1,13 @@
+    int gameOfXor(int N , int A[]) {
+        // code here
+        int ans = 0;
+        
+        for(int i=0; i<N; i++)
+        {
+            if( ((i+1) & 1) and ((N-i) & 1) )
+            {
+                ans ^= A[i];
+            }
+        }
+        return ans;
+    } 
