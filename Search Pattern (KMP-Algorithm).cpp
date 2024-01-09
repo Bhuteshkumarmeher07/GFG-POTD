710ae388503d62a1d4e565183f56278115540711
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-01-09 11:19:30 +0530

    Create Search Pattern (KMP-Algorithm).cpp

---
+++ Search Pattern (KMP-Algorithm).cpp
@@ -0,0 +1,15 @@
+        vector <int> search(string pat, string txt)
+        {
+           vector<int>ans;
+           size_t pos = txt.find(pat);
+           while(pos!=string::npos)
+           {
+               ans.push_back(pos+1);
+               pos = txt.find(pat,pos+1);
+           }
+           if(ans.empty())
+           {
+               ans.push_back(-1);
+           }
+           return ans;
+        }
