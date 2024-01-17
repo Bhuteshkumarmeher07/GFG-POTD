08703b11eaf9e3850e9158bfd23c4d34e8c8abb3
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-01-17 17:03:08 +0530

    Create All Unique Permutations of an array.cpp

---
+++ All Unique Permutations of an array.cpp
@@ -0,0 +1,21 @@
+  public:
+  set<vector<int>>st;
+  void perm(vector<int>temp,int l,int h){
+      if(l==h){
+          st.insert(temp);
+        }
+      else{
+          for(int i=l;i<=h;i++){
+              swap(temp[l],temp[i]);
+              perm(temp,l+1,h);
+              swap(temp[l],temp[i]);
+            }
+        }
+    }    
+  
+    vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
+        perm(arr,0,n-1);
+        vector<vector<int>>ans;
+        ans.insert(ans.begin(),st.begin(),st.end());
+        return ans;
+    }
