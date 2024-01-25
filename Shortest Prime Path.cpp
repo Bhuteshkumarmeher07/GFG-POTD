e4c48ad720dff1413ffeea9c92b0e3f3af007d5d
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-01-25 14:35:51 +0530

    Create Shortest Prime Path.cpp

---
+++ Shortest Prime Path.cpp
@@ -0,0 +1,52 @@
+public:
+    bool p[N];
+    void buildSeive(){
+        for(int i=2; i<=N; i++)
+            p[i] = true;
+
+        for(int i=2; i*i<=N; i++)
+            if(p[i]){
+                for(int j=i*i; j<=N; j+=i)
+                    p[j] = false;
+            }
+    }
+
+    int solve(int n1,int n2){   
+        // n1 --> n2
+        queue<pair<int,int>> q; // {Pnum, dist}
+        bool vis[N] = {0};
+
+        q.push({n1,0});
+        vis[n1] = true;
+        int minDist = 1e8;
+        buildSeive();
+
+        while(!q.empty()){
+            auto it = q.front();
+            int n = it.first;
+            int dist = it.second;
+            q.pop();
+
+            if(n==n2){
+                minDist = min(minDist, dist);
+                if(minDist==0)
+                    break;
+            }
+
+            for(int d=1; d<=n; d*=10){
+                int dig = (n/d)%10;
+                for(int i=0; i<=9; i++){
+                    if((i==0 && d==4) || i==dig)
+                        continue;
+                    int num = n - dig*d + i*d;
+                    if(num>=1000 && p[num] && !vis[num]){
+                        vis[num] = true;
+                        q.push({num, dist+1});
+                    }
+                }
+            }
+        }
+
+        return minDist;
+
+    }
