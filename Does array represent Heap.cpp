1a33828723100dde1bc00bbf4cacfd9f3535d88d
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-02-17 12:15:08 +0530

    Create Does array represent Heap.cpp

---
+++ Does array represent Heap.cpp
@@ -0,0 +1,10 @@
+bool isMaxHeap(int arr[], int n)
+    {
+        for(int i = 0; i < (n/2) ; i++){
+            if(arr[i] < arr[2*i+1]) 
+                return false;
+            if(2*i+2 < n)if(arr[i] < arr[2*i+2])
+                return false;
+        }
+        return true;
+    }
