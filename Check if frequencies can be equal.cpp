fdac91a5cfa81d679df16a738d4f84332a0adcc5
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-03-08 20:09:26 +0530

    Create Check if frequencies can be equal.cpp

---
+++ Check if frequencies can be equal.cpp
@@ -0,0 +1,40 @@
+bool sameFreq(string s)
+	{
+	    // code here 
+	     unordered_map<char, int> mp;
+        for (char ch : s) {
+            mp[ch]++;
+        }
+        vector<int> v;
+        for (auto i : mp) {
+            v.push_back(i.second);
+        }
+        int cnt = 0;
+        sort(v.begin(), v.end());
+        int n = v.size();
+        if(n== 1){
+            return true;
+        }
+        for(int i=0; i<n-1; i++){
+            if(i==0 && v[i] == 1){
+                continue;
+            }
+            if(v[i] != v[i+1]){
+                break;
+            }
+            if(i == n-2 && v[i] == v[i+1]){
+                return true;
+            }
+        }
+        
+        for(int i =0; i<n-1; i++){
+            if(i<n-2 && v[i] != v[i+1]){
+                break;
+            }
+            if(i == n-2 && (v[i] == v[i+1] || v[i]+1 == v[i+1])){
+                return true;
+            }
+        }
+        
+        return false;
+	}
