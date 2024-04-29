5959ff5bdf94b2e93cea36330df1e023420b0524
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-04-29 10:57:47 +0530

    Create Remove every kth node.cpp

---
+++ Remove every kth node.cpp
@@ -0,0 +1,17 @@
+Node* deleteK(Node *head,int K){
+      //Your code here
+      Node* pre = nullptr;
+      Node* temp = head;
+      if(K == 1) return nullptr;
+      int cnt = 1;
+      while(temp){
+          if(cnt == K) {
+              pre->next = temp->next;
+              cnt = 0;
+          }
+          pre = temp;
+          temp = temp->next;
+          cnt++;
+      }
+      return head;
+    }
