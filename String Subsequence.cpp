bdbd7d861c8feaff39ca2d689498a47c66a17942
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-05-30 21:07:13 +0530

    Create String Subsequence.cpp

---
+++ String Subsequence.cpp
@@ -0,0 +1,24 @@
+int mod = (int) 1e9 + 7;
+int countWays(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        vector<int> prev(m + 1), curr(m + 1);
+        
+        prev[0] = curr[0] = 1;
+            
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                int count = 0;
+                
+                if(s1[i - 1] == s2[j - 1])
+                    count = (count + prev[j - 1] + prev[j]) % mod;
+                else
+                    count = (count + prev[j]) % mod;
+                    
+                curr[j] = count % mod;
+            }
+            
+            prev = curr;
+        }
+        
+        return prev[m];
+    }
