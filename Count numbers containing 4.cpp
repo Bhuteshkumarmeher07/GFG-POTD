731e4cfcdff52584398f01f58ac2bb4e97c67f00
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-06-12 12:03:13 +0530

    Create Count numbers containing 4.cpp

---
+++ Count numbers containing 4.cpp
@@ -0,0 +1,23 @@
+int countNumberswith4(int n) {
+        int count=0;
+        int i=4;
+        if(n<i)
+        {
+            return 0;
+        }
+        for(int i=4;i<=n;i++)
+        {
+            int a=i;
+            while(a>0){
+            if(a%10==4){
+            count++;
+            break;
+            }
+            else
+            {
+                a/=10;
+            }
+            }   
+        }
+        return count;
+    }
