e5e52e493afff5fd30a1d7e534938bdaecdbfa16
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-07-06 22:55:40 +0530

    Create Populate Inorder Successor for all nodes.cpp

---
+++ Populate Inorder Successor for all nodes.cpp
@@ -0,0 +1,12 @@
+Node* prev = NULL;
+
+    void populateNext(Node *root) {
+        // code here
+        if(root==NULL) return;
+        populateNext(root->left);
+        if(prev!=NULL){
+            prev->next = root;
+        }
+        prev = root;
+        populateNext(root->right);
+    }
