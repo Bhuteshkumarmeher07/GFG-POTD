9f2f6cc2fc29639f2f435804afff78139ca1db0f
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-07-07 19:39:10 +0530

    Create Ancestors in Binary Tree.cpp

---
+++ Ancestors in Binary Tree.cpp
@@ -0,0 +1,19 @@
+vector<int>ans;
+    bool solve(Node* root, int t){
+        if(root==NULL) return 0;
+        if(root->data==t) return 1;
+        bool lft=solve(root->left, t);
+        bool rgt=solve(root->right, t);
+        if(lft||rgt){
+            ans.push_back(root->data);
+            return 1;
+        }
+        return 0;
+    }
+    
+    vector<int> Ancestors(struct Node *root, int target) {
+        // Code here
+        //vector<int>ans;
+        solve(root, target);
+        return ans;
+    }
