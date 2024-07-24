8a149b420ae816bc2d860e9d90ca0a86aa091f83
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-07-24 09:25:52 +0530

    Create Check for BST.cpp

---
+++ Check for BST.cpp
@@ -0,0 +1,18 @@
+bool isBST(Node* root) {
+        // Your code here
+        return checkBST(root,INT_MIN,INT_MAX);
+    }
+    
+    bool checkBST(Node* root, long min, long max){
+    
+        if(root==NULL){
+            return true;
+        }
+        
+        if(root->data<=min || root->data>=max){
+            return false;
+        }
+        
+        return checkBST(root->left,min,root->data) && checkBST(root->right,root->data,max);
+    
+    }
