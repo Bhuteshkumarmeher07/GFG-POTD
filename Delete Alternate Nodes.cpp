d9a0426269e13561efcc7c5fc52f77a703851d73
Bhutesh  kumar meher <98634733+Bhuteshkumarmeher07@users.noreply.github.com>
2024-10-13 08:17:53 +0530

    Create Delete Alternate Nodes.cpp

---
+++ Delete Alternate Nodes.cpp
@@ -0,0 +1,10 @@
+void deleteAlt(struct Node *head) {
+      Node* temp  =head; 
+      while(head && head->next){
+          Node* del = head->next; 
+          head->next = head->next->next; 
+          delete del; 
+          head  = head->next; 
+      }
+    }
+}
